404a690a911306b844daa05f52d3cafad98d8805
FILIPE RIBEIRO DE SOUZA <filipe.ribeiro@SETELAGOAS.MG>
2023-07-03 14:02:58 -0300

    exercicio modulo 22

--- FrontEnd/Webpack/src/index.js
+++ FrontEnd/Webpack/src/index.js
@@ -1,20 +1,24 @@
 import './styles.scss'
-
 import img from './matrix.png'
 
+function rootStyle(){
+    const elemRoot = document.getElementById('root')
+    elemRoot.classList.add('container')
+}
 
 function titleCompontent(){
     const elemH1 = document.createElement('h1')
-    elemH1.innerHTML = 'Hello World 2'
+    elemH1.innerHTML = 'Hello World 10'
     elemH1.classList.add('title')
     return elemH1
 }
 
 function imageComponent(){
-    const elemImg = new image(1200,800)
+    const elemImg = new Image(1200,800)
     elemImg.src = img
     return elemImg
 }
 
-document.body.appendChild(titleCompontent())
-document.body.appendChild(imageComponent())
+rootStyle()
+document.getElementById('root').appendChild(titleCompontent())
+document.getElementById('root').appendChild(imageComponent())
