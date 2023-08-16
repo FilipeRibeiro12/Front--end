678d8be8ce2e43bf0693cc8a60004d44b05282bd
FilipeRibeiro12 <lipi1702@gmail.com>
2023-08-16 10:18:00 -0300

    16/08

---
+++ FrontEnd/JavaScript-Advanced/src/modulesExports.js
@@ -0,0 +1,12 @@
+// Named exports
+export let cores = []
+export function getName(){}
+export class Animais {}
+
+
+// Lista de exportação
+export {cores, getName, Animais}
+
+// Strict mode , como ficam todos os exports
+
+/*export default é apenas um por arquivo */export default getCars()
---
+++ FrontEnd/JavaScript-Advanced/src/modulesImports.js
@@ -0,0 +1,2 @@
+import bootstrap from 'bootstrap'
+import * as nomeQualquer from './modulesExports'
